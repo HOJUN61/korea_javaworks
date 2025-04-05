87abdc9ab9b922d9606b370cb9bfb451f3c74774
HOJUN61 <hojungim91@gmail.com>
2025-04-05 15:24:16 +0900

    array

---
+++ Chapter3/src/nestedfor/ForForEx.java
@@ -0,0 +1,34 @@
+package nestedfor;
+
+public class ForForEx {
+
+	public static void main(String[] args) {
+		// 중첩 for문
+		// 5행 5열을 만든다
+		for(int i = 1; i <= 5 ; i ++) {
+			for(int j = 1; j <= 5; j++) {
+				System.out.print("나");
+			}
+			System.out.println(); // 행바꿈
+		}
+
+		/*
+		 연속된 숫자 채우기
+		 1 2 3 4 5
+		 6 7 8 9 10
+		 11 12 13 14 15
+		 */
+		for(int i = 0; i <= 4 ; i ++) {
+			for(int j = 1; j <= 5; j++) {
+				int num = i*5+j;
+				if(num > 23)
+					break;
+				System.out.print(i*5+j + " ");
+			}
+			System.out.println();
+		}
+		System.out.println(); // 행바꿈
+	}
+}
+
+
---
+++ Practice/src/chap3/ArrayTest.java
@@ -0,0 +1,23 @@
+package chap3;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// 배열의 길이가 5인 정수 배열을 선언하고, 1 ~ 10중 짝수만을 배열에 저장한 후 
+		// 그 합과 평균을 계산하세요.
+		int total = 0;
+		double avg;
+		
+		int[] arr = new int[] {2, 4, 6, 8, 10};
+		
+//		int[] arr = {2, 4, 6, 8, 10};
+		for(int i = 0; i < arr.length; i++) {
+			total += arr[i];
+		}
+		avg = (double)total / arr.length;
+		System.out.println("합계 : " + total);
+		System.out.println("평균 : " + avg);
+
+	}
+
+}
